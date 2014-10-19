184898030d9a84ea0a7ead603b5d3b20781e3d12
Robert Rees <shudderfix@gmail.com>
2014-10-19 23:27:55 +0100

    Kind of gets ImmutableJS into the mix, probably the wrong way though

--- public/todo/todo.js
+++ public/todo/todo.js
@@ -1,5 +1,14 @@
 angular.module('demoApp', [])
 	.controller('TodoController', ['$scope', function($scope) {
+
+		var todoItems = null;
+
+		require(['https://cdn.jsdelivr.net/immutable.js/2.5.0/Immutable.min.js'], function(immutable) {
+			console.log(immutable);
+
+			console.log(todoItems);
+		});
+
 		$scope.remaining = function() {
 			return 0;
 		}
