af9adfe450d0813720d3da2501bba39fb840edaf
Robert Rees <shudderfix@gmail.com>
2015-09-20 23:46:25 +0100

    Starts to use some ES2015 to lighten the code and adds some model data

--- public/password/app.js
+++ public/password/app.js
@@ -3,12 +3,32 @@ angular.module('passwordApp', [])
         var self = this;
         self.hello = "Hello world"
     }])
-    .directive('passwordPicker', [function() {
+    .directive('passwordPicker', [() => {
+            const template = `
+            <section>
+                <div>
+                    <input type="text" ng-value="password">
+                </div>
+
+                <div>
+                    <select ng-model="category" ng-options="cat.label for cat in categories">
+                    </select>
+                </div>
+            </section>
+            `;
+
             return {
-                templateUrl: 'password-directive.html',
+                template: template,
                 restrict: 'E',
-                link: function($scope, $element, $attrs) {
-                    $scope.password = "Not yet generated"
+                link: ($scope, $element, $attrs) => {
+                    $scope.password = "Not yet generated";
+                    $scope.categories = [
+                        {
+                            label: "Animals",
+                            value: 'animals'
+                        },
+                    ];
+                    $scope.category = $scope.categories[0];
                 },
                 scope: {
 
